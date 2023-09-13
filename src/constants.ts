994e22ecb655c2c60223560a99430cd52d260dac
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-09-13 16:10:56 +0100

    feat: remove payment percentages constants

--- src/constants.ts
+++ src/constants.ts
@@ -32,11 +32,6 @@ export const MARKETPLACE_FEE = '0.5'; // u
 export const SCRIPT_CREATION_FEE = '0.5'; // u
 export const OPERATOR_REGISTRATION_AR_FEE = '0.05'; // u
 
-export const OPERATOR_PERCENTAGE_FEE = 0.7;
-export const MARKETPLACE_PERCENTAGE_FEE = 0.1;
-export const CURATOR_PERCENTAGE_FEE = 0.05;
-export const CREATOR_PERCENTAGE_FEE = 0.15;
-
 export const TAG_NAMES = {
   appName: 'App-Name',
   appVersion: 'App-Version',
