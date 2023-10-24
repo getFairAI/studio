b9c1eb9c1f7d26bfc5598a720e3b68fe9c195006
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-10-24 15:55:29 +0100

    feat: add withdraw functionality

--- src/components/fund-dialog.tsx
+++ src/components/fund-dialog.tsx
@@ -27,23 +27,166 @@ import {
   IconButton,
   InputAdornment,
   InputLabel,
+  List,
+  ListItem,
   MenuItem,
   Select,
   SelectChangeEvent,
+  Slider,
+  Tab,
+  Tabs,
   TextField,
   Typography,
 } from '@mui/material';
-import { ChangeEvent, Dispatch, SetStateAction, useContext, useEffect, useState } from 'react';
+import { ChangeEvent, Dispatch, SetStateAction, SyntheticEvent, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import RefreshIcon from '@mui/icons-material/Refresh';
-import { NODE2_BUNDLR_URL } from '@/constants';
-import { BundlrContext, bundlrNodeUrl } from '@/context/bundlr';
+import { NODE1_IRYS_URL, NODE2_IRYS_URL } from '@/constants';
+import { IrysContext, irysNodeUrl } from '@/context/irys';
 import { useSnackbar } from 'notistack';
 import { WalletContext } from '@/context/wallet';
 import arweave from '@/utils/arweave';
-import { NumericFormat } from 'react-number-format';
+import { NumberFormatValues, NumericFormat } from 'react-number-format';
 import DebounceLoadingButton from './debounce-loading-button';
 
 type FundFinishedFn = (node: string) => Promise<void>;
+type tabOptions = 'fund' | 'withdraw';
+
+const maxPercentage = 100;
+const marks = [
+  {
+    value: 0,
+    label: '0%',
+  },
+  {
+    value: 25,
+    label: '25%',
+  },
+  {
+    value: 50,
+    label: '50%',
+  },
+  {
+    value: 75,
+    label: '75%',
+  },
+  {
+    value: maxPercentage,
+    label: '100%',
+  },
+];
+
+const valueLabelFormat = (val: number) => `${val}%`;
+
+const WithdrawPanel = ({ currentTab }: { currentTab: tabOptions }) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const { withdrawNode, nodeBalance } = useContext(IrysContext);
+  const [ percentage, setPercentage ] = useState(0);
+  const [ withdrawAmount, setWithdrawAmount ] = useState(0);
+  const [ loading, setLoading ] = useState(false);
+  const parsedNodeBalance = useMemo(() => Number(arweave.ar.winstonToAr(nodeBalance.toString())), [ nodeBalance ]);
+
+  const handleMaxClick = useCallback(() => setWithdrawAmount(parsedNodeBalance), [setWithdrawAmount, parsedNodeBalance]);
+
+  const isAllowed = useCallback(
+    (val: NumberFormatValues) => !val.floatValue || val?.floatValue <= parsedNodeBalance,
+    [parsedNodeBalance],
+  );
+
+  const handleAmountChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      if (event.target.value === '') {
+        setWithdrawAmount(0);
+        setPercentage(0);
+      } else {
+        setWithdrawAmount(Number(event.target.value));
+        const newPercentage = (Number(event.target.value) / parsedNodeBalance) * maxPercentage;
+        setPercentage(Math.round(newPercentage));
+      }
+    },
+    [setWithdrawAmount],
+  );
+
+  const handleSliderChange = useCallback(
+    (_event: Event, newValue: number | number[]) => {
+      setWithdrawAmount((parsedNodeBalance * (newValue as number)) / maxPercentage);
+      setPercentage(newValue as number);
+    },
+    [setWithdrawAmount, parsedNodeBalance],
+  );
+
+  const handleWithdraw = useCallback(async () => {
+    try {
+      setLoading(true);
+      const winstonAmount = arweave.ar.arToWinston(withdrawAmount.toString());
+  
+      const res = await withdrawNode(winstonAmount);
+      setWithdrawAmount(0);
+      setPercentage(0);
+      enqueueSnackbar(
+        <>
+          Withdrew Balance from Irys Node: {arweave.ar.winstonToAr(res.requested.toString())} AR.
+          <br></br>
+          <a href={`https://viewblock.io/arweave/tx/${res.tx_id}`} target={'_blank'} rel='noreferrer'>
+            <u>View Transaction in Explorer</u>
+          </a>
+        </>,
+        { variant: 'success' },
+      );
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      enqueueSnackbar(`Error: ${error}`, { variant: 'error' });
+    }
+  }, [ arweave, withdrawAmount, setLoading, setWithdrawAmount, enqueueSnackbar, withdrawNode ]);
+
+  return <Box role='tabpanel' hidden={currentTab !== 'withdraw'} display={'flex'} flexDirection={'column'} gap={'16px'}>
+    {currentTab === 'withdraw' && (<>
+      <Box display={'flex'} padding={'0 16px'} gap={'24px'} alignItems={'center'}>
+        <Slider
+          onChange={handleSliderChange}
+          disabled={nodeBalance <= 0}
+          marks={marks}
+          value={percentage}
+          step={1}
+          min={0}
+          getAriaValueText={valueLabelFormat}
+          valueLabelFormat={valueLabelFormat}
+          valueLabelDisplay='auto'
+        />
+        <NumericFormat
+          label='Amount to Withdraw'
+          placeholder='Amount to Withdraw'
+          value={withdrawAmount}
+          onChange={handleAmountChange}
+          customInput={TextField}
+          helperText={
+            <Typography sx={{ cursor: 'pointer' }} variant='caption'>
+              <u>Max: {parsedNodeBalance.toFixed(4)}</u>
+            </Typography>
+          }
+          FormHelperTextProps={{
+            onClick: handleMaxClick,
+          }}
+          allowNegative={false}
+          isAllowed={isAllowed}
+          margin='dense'
+          decimalScale={4}
+          disabled={nodeBalance <= 0}
+        />
+      </Box>
+      <Box>
+        <DebounceLoadingButton
+          loading={loading}
+          variant='outlined'
+          onClick={handleWithdraw}
+          disabled={withdrawAmount <= 0 || withdrawAmount >= nodeBalance}
+        >
+          Withdraw
+        </DebounceLoadingButton>
+      </Box>
+    </>)}
+  </Box>;
+};
 
 const FundDialog = ({
   open,
@@ -54,17 +197,18 @@ const FundDialog = ({
   setOpen: Dispatch<SetStateAction<boolean>>;
   handleFundFinished?: FundFinishedFn;
 }) => {
-  const [node, setNode] = useState<bundlrNodeUrl>(NODE2_BUNDLR_URL);
+  const [node, setNode] = useState<irysNodeUrl>(NODE2_IRYS_URL);
   const [amount, setAmount] = useState(0);
   const [loading, setLoading] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
-  const { nodeBalance, fundNode, updateBalance, changeNode } = useContext(BundlrContext);
+  const { nodeBalance, fundNode, updateBalance, changeNode } = useContext(IrysContext);
   const { currentBalance: walletBalance, updateBalance: updateWalletBalance } =
     useContext(WalletContext);
+  const [ currentTab, setCurrentTab ] = useState<'fund' | 'withdraw'>('fund');
 
   const handleChange = async (event: SelectChangeEvent) => {
-    setNode(event.target.value as bundlrNodeUrl);
-    await changeNode(event.target.value as bundlrNodeUrl);
+    setNode(event.target.value as irysNodeUrl);
+    await changeNode(event.target.value as irysNodeUrl);
   };
 
   const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -101,7 +245,7 @@ const FundDialog = ({
       setAmount(0);
       enqueueSnackbar(
         <>
-          Funded Bundlr with {arweave.ar.winstonToAr(res.quantity)} AR.
+          Funded Irys node with {arweave.ar.winstonToAr(res.quantity)} AR.
           <br></br>
           <a href={`https://viewblock.io/arweave/tx/${res.id}`} target={'_blank'} rel='noreferrer'>
             <u>View Transaction in Explorer</u>
@@ -117,6 +261,10 @@ const FundDialog = ({
     }
   };
 
+  const handleTabChange = useCallback((_: SyntheticEvent, value: 'fund' | 'withdraw') => {
+    setCurrentTab(value);
+  }, [ setCurrentTab ]);
+
   return (
     <>
       <Dialog open={open} maxWidth={'sm'} fullWidth onClose={handleClose}>
@@ -128,49 +276,61 @@ const FundDialog = ({
               lineHeight: '31px',
             }}
           >
-            Fund Bundlr Node
+            Irys Node Settings
           </Typography>
         </DialogTitle>
         <DialogContent>
           <Alert variant='outlined' severity='info' sx={{ marginBottom: '16px' }}>
             <Typography>
-              Funding a Node Bundlr can take up to 40 minutes. Current Pending transactions will not
+              Funding or Withdrawing balances from a Node can take up to 40 minutes. Current Pending transactions will not
               be reflected on the node balance until they are confirmed.
               <br />
-              You can view Bundlr Node transactions at:
-              <br />
-              <a
-                href='https://viewblock.io/arweave/address/OXcT1sVRSA5eGwt2k6Yuz8-3e3g9WJi5uSE99CWqsBs'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <u>
-                  https://viewblock.io/arweave/address/OXcT1sVRSA5eGwt2k6Yuz8-3e3g9WJi5uSE99CWqsBs
-                </u>
-              </a>
+              You can view Irys Node transactions at:
             </Typography>
+            <List sx={{ listStyle: 'inside', padding: 0 }}>
+              <ListItem sx={{ display: 'list-item' }}>
+                <a
+                  href='https://viewblock.io/arweave/address/OXcT1sVRSA5eGwt2k6Yuz8-3e3g9WJi5uSE99CWqsBs'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <u>
+                    https://viewblock.io/arweave/address/OXcT1sVRSA5eGwt2k6Yuz8-3e3g9WJi5uSE99CWqsBs
+                  </u>
+                </a>
+              </ListItem>
+              <ListItem sx={{ display: 'list-item' }}>
+                <a
+                  href='https://viewblock.io/arweave/address/ZE0N-8P9gXkhtK-07PQu9d8me5tGDxa_i4Mee5RzVYg'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <u>
+                    https://viewblock.io/arweave/address/ZE0N-8P9gXkhtK-07PQu9d8me5tGDxa_i4Mee5RzVYg
+                  </u>
+                </a>
+              </ListItem>
+            </List>
           </Alert>
           <Box
             display={'flex'}
             flexDirection={'column'}
             justifyContent={'space-evenly'}
-            marginBottom={'8px'}
           >
             <FormControl fullWidth margin='dense'>
-              <InputLabel id='select-label'>Bundlr Node</InputLabel>
+              <InputLabel id='select-label'>Irys Node</InputLabel>
               <Select
                 labelId='select-label'
                 value={node}
                 onChange={handleChange}
-                label={'Bundlr Node'}
-                disabled
+                label={'Irys Node'}
               >
-                {/* <MenuItem value={DEV_BUNDLR_URL}>dev.bundlr.network</MenuItem> */}
-                <MenuItem value={NODE2_BUNDLR_URL}>node2.bundlr.network</MenuItem>
+                <MenuItem value={NODE1_IRYS_URL}>node1.irys.xyz</MenuItem>
+                <MenuItem value={NODE2_IRYS_URL}>node2.irys.xyz</MenuItem>
               </Select>
             </FormControl>
             <TextField
-              label='Current Node Balance'
+              label='Current Irys Balance'
               value={arweave.ar.winstonToAr(nodeBalance.toString())}
               disabled
               margin='dense'
@@ -184,43 +344,55 @@ const FundDialog = ({
                 ),
               }}
             />
-            <NumericFormat
-              value={amount}
-              onChange={handleAmountChange}
-              customInput={TextField}
-              helperText={
-                <Typography sx={{ cursor: 'pointer' }} variant='caption'>
-                  <u>Max: {walletBalance.toFixed(4)}</u>
-                </Typography>
-              }
-              FormHelperTextProps={{
-                onClick: () => setAmount(walletBalance),
-              }}
-              allowNegative={false}
-              isAllowed={(val) => !val.floatValue || val?.floatValue < walletBalance}
-              margin='dense'
-              decimalScale={4}
-              decimalSeparator={'.'}
-            />
-          </Box>
-          <Box>
-            <DebounceLoadingButton
-              loading={loading}
-              variant='outlined'
-              onClick={handleFund}
-              disabled={amount <= 0 || amount >= walletBalance}
-            >
-              Fund
-            </DebounceLoadingButton>
-            {handleFundFinished && (
-              <Button
-                onClick={() => handleFundFinished(node)}
-                variant='contained'
-                disabled={nodeBalance <= 0}
-              >
-                Continue
-              </Button>
-            )}
+            <Tabs value={currentTab} onChange={handleTabChange}>
+              <Tab label='Fund' value='fund' />
+              <Tab label='Withdraw' value='withdraw' />
+            </Tabs>
+            <Box role='tabpanel' hidden={currentTab !== 'fund'} display={'flex'} flexDirection={'column'} gap={'16px'}>
+              {currentTab === 'fund' && (<>
+                <NumericFormat
+                  value={amount}
+                  onChange={handleAmountChange}
+                  customInput={TextField}
+                  helperText={
+                    <Typography sx={{ cursor: 'pointer' }} variant='caption'>
+                      <u>Max: {walletBalance.toFixed(4)}</u>
+                    </Typography>
+                  }
+                  FormHelperTextProps={{
+                    onClick: () => setAmount(walletBalance),
+                  }}
+                  allowNegative={false}
+                  margin='dense'
+                  decimalScale={4}
+                  isAllowed={(val) => !val.floatValue || val?.floatValue < walletBalance}
+                  decimalSeparator={'.'}
+                  sx={{
+                    width: '100%'
+                  }}
+                />
+                <Box>
+                  <DebounceLoadingButton
+                    loading={loading}
+                    variant='outlined'
+                    onClick={handleFund}
+                    disabled={amount <= 0 || amount >= walletBalance}
+                  >
+                    Fund
+                  </DebounceLoadingButton>
+                  {handleFundFinished && (
+                    <Button
+                      onClick={() => handleFundFinished(node)}
+                      variant='contained'
+                      disabled={nodeBalance <= 0}
+                    >
+                      Continue
+                    </Button>
+                  )}
+                </Box>
+              </>)}
+            </Box>
+            <WithdrawPanel currentTab={currentTab}/>
           </Box>
         </DialogContent>
       </Dialog>
